bd25a036e1b1d68fb3e6b345a7e1c5e71e8a4099
Makh Vitalii <patriot02faqq@gmail.com>
2017-10-28 19:18:48 +0300

    delete servlet

--- src/main/java/com/makh/servlets/DeleteServlet.java
+++ src/main/java/com/makh/servlets/DeleteServlet.java
@@ -1,11 +1,18 @@
 package com.makh.servlets;
 
+import com.makh.beans.Administrator;
+import com.makh.dao.AbstractDao;
+import com.makh.dao.DaoException;
+import com.makh.dao.DaoFactory;
+import com.makh.mysql.MySqlDaoFactory;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 // Created by on 14.10.2017.
 @WebServlet(name = "DeleteServlet")
@@ -15,6 +22,19 @@ public class DeleteServlet extends HttpServlet {
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html");
+        PrintWriter writer = response.getWriter();
+        Integer id = Integer.parseInt(request.getParameter("id"));
+        DaoFactory factory = new MySqlDaoFactory();
 
+        try {
+            AbstractDao dao = factory.getDao(factory.getConnection(), Administrator.class);
+            Administrator user = (Administrator) dao.read(id);
+            dao.delete(user);
+            writer.print("User " + user.getName() + " " + user.getSurname() + " deleted");
+            request.getRequestDispatcher("view-users.jsp").include(request,response);
+        } catch (DaoException e) {
+            e.printStackTrace();
+        }
     }
 }
